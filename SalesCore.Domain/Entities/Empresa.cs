2c774d16d5d2ba6b5fd7466cb600ec4593aa5f7d
Aluno <Aluno@LAB-607>
2026-09-03 21:23:22 -0300

    Adicionar arquivos de projeto.

---
+++ SalesCore.Domain/Entities/Empresa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCore.Domain.Entities
+{
+    internal class Empresa
+    {
+        public string CNPJ { get; set; }
+        public string razaoSocial { get; set; }
+        public string nomeFantasia { get; set; }
+        public string email { get; set; }
+        public string telefone { get; set; }
+        public DateTime dataDoCadastro { get; set; }
+        public Boolean ativo { get; set; }
+    }
+}
